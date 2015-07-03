4270250ddef22a9d9cf71958ec65b4e90f0e1c60
klawisz1313 <klawisz1313@users.noreply.github.com>
2015-07-03 17:57:56 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -118,6 +118,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://raw.githubusercontent.com/klawisz1313/basicBot-customization/master/extension.js", extend);
+    $.getScript("https://rawgit.com/Yemasthui/basicBot/master/basicBot.js", extend);
 
 }).call(this);
