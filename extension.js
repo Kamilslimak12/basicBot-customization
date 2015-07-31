c450aca120c27dbbfa4935c3bf28bbcffe167964
klawisz1313 <klawisz1313@users.noreply.github.com>
2015-07-31 23:01:28 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -105,6 +105,20 @@
                 }
             }
         };
+        
+         bot.commands.RCSKomenda = { //ogólnie musi być bot.commands.jakasunikalnanazwa = {
+            command: 'instalacja', //twoja nazwa komendy, bez !
+            rank: 'user', //poziom użytkownika do jej odpalenia
+            type: 'exact', //czy komenda może być wywolywana z argumentami (start with) czy bez (exact), raczej zostaw jak jest
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0); //nie odpala komendy jesli jest argument, a wybrano ze nie
+                if (!bot.commands.executable(this.rank, chat)) return void(0); //nie odpala komendy jesli uzytkownik nie ma odpowiedniej rangi
+                else {
+                    //kod jaki bot wykonuje, dowolny JS, korzystaj z front-end API plugdj
+                    API.sendChat("Poradnik przedstawiający jak zainstalować RCS na przykładzie Mozilli. W przypadku innych przeglądarek (prócz Google Chrome, na które istnieje oficjalne rozszerzenie) postępuje się analogicznie. http://i.imgur.com/2x79L18.gif"); //wysyla wiadomosc
+                }
+            }
+        };
 
         //i tyle :v
 
