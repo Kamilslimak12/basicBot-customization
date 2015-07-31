74b35836a41a1823a94b347ef921a08892f13ace
klawisz1313 <klawisz1313@users.noreply.github.com>
2015-07-31 21:40:36 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -89,6 +89,22 @@
                 }
             }
         };
+        bot.commands.praca = { //ogólnie musi być bot.commands.jakasunikalnanazwa = {
+            command: 'praca',  //twoja nazwa komendy, bez !
+            rank: 'manager', //poziom użytkownika do jej odpalenia
+            type: 'exact', //czy komenda może być wywolywana z argumentami (start with) czy bez (exact), raczej zostaw jak jest
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0); //nie odpala komendy jesli jest argument, a wybrano ze nie
+                if (!bot.commands.executable(this.rank, chat)) return void (0); //nie odpala komendy jesli uzytkownik nie ma odpowiedniej rangi
+                else {
+                        //kod jaki bot wykonuje, dowolny JS, korzystaj z front-end API plugdj
+                    setTimeout(API.sendChat("!historyskip"), 5000); //wysyla wiadomosc
+                    setTimeout(API.sendChat("!timeguard"), 5000); //wysyla wiadomosc
+                    setTimeout(API.sendChat("!togglebl"), 5000); //wysyla wiadomosc
+                   
+                }
+            }
+        };
 
 	//i tyle :v
 	
