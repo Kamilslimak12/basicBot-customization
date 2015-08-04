1fda7c679c441006541c79ff2f4b3d8f1a0ee567
klawisz1313 <klawisz1313@users.noreply.github.com>
2015-08-04 14:26:09 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -197,7 +197,7 @@ bot.commands._nightmode = {
         afkRankCheck: "ambassador",
         motdEnabled: false,
         motdInterval: 10,
-        motd: "Trzy szybkie poradniki jak rozwiązać problem z niedziałającym plugiem: Po pierwsze zmiana DNS na te od googla http://www.download.net.pl/jak-zmienic-serwery-dns-na-serwery-od-google/n/2161/ Jak nie pomoze to: http://i.imgur.com/0AezrQI.gif http://i.imgur.com/CSihaKA.gif",
+        motd: "Trzy szybkie poradniki jak rozwiązać problem z niedziałającym plugiem: Po pierwsze zmiana DNS na te od googla http://bit.ly/1K17w1m Jak nie pomoze: http://bit.ly/1M2wWjL http://bit.ly/1KNJkoQ",
         filterChat: false,
         etaRestriction: true,
         welcome: false,
