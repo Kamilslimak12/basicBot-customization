0b40f53d239f08745d78412f02a43a581812689f
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-08-20 10:47:20 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -57,7 +57,19 @@
                 }
             }
         };
-
+  
+        bot.commands.rammusmode = {
+            command: 'rammusmode',  
+            rank: 'bouncer', 
+            type: 'exact', 
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me This chat is now in Rammus mode, type :nb3ok: to continue.");
+                }
+            }
+        };
 
 
 
