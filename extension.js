011c557098d6f86ce28538fc80125d87248a5fc5
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-08-23 15:17:41 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -90,7 +90,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                 if (!bot.commands.executable(this.rank, chat)) return void(0);
                 else {
-                    API.sendChat("/me Poradnik do pluga w wersji dla opornych http://i.imgur.com/IG95LvA.jpg");
+                    API.sendChat("/me Poradnik do pluga w wersji dla opornych http://bit.ly/1PDu39i");
                 }
             }
         };
