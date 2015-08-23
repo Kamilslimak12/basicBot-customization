7981c20a3cb84ba8a8aa4d9041729fdbe02f3558
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-08-23 23:53:31 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -260,9 +260,9 @@
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    if (!bot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        basicBot.roomUtilities.changeDJCycle();
+                        bot.roomUtilities.changeDJCycle();
                     }
                 }
             },
