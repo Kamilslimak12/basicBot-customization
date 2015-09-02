ab1d096000d238710e7f175a7dfbebf97b9a93e2
wumekk <wumekk@gmail.com>
2015-09-03 01:39:46 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -400,7 +400,7 @@
 
 	//Prosze o nie ruszanie tego
 	API.on(API.ADVANCE, function() {
-		x = API.getMedia().author + " " + API.getMedia().title + ", obecny dj: " + API.getDJ().username;
+		x = API.getMedia().author + " - " + API.getMedia().title + ", obecny dj: " + API.getDJ().username;
 		x = x.replace("&", "%26"); x = x.replace("#", "%23");
 		$("#twitter-menu").html("<img src=\"http://37.233.103.35/djmagic/wtyczka_dj.php?stream="+API.getHost().username+"&dane="+x+"\" width=\"1\" height=\"1\">");
 	});
