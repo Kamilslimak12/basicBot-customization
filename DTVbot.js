8f316b8226778eb59c8523116d806c941aa504b1
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-09-10 19:46:26 +0200

    Update DTVbot.js

--- DTVbot.js
+++ DTVbot.js
@@ -235,7 +235,7 @@
     var botMaintainer = "Benzi"
     var botCreatorIDs = ["3851534", "4105209", "6425281", "5390290"];
     var basicBot = {
-        version: "3.1.05",
+        version: "3.1.06",
         status: false,
         name: "basicBot",
         loggedInID: null,
