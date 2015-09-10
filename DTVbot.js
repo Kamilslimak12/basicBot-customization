4b738b29708cdf748d416826cfcad56ba2d151ed
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-09-10 18:34:07 +0200

    Update DTVbot.js

--- DTVbot.js
+++ DTVbot.js
@@ -376,13 +376,12 @@
                     var ind = Math.floor(Math.random() * basicBot.room.roulette.participants.length);
                     var winner = basicBot.room.roulette.participants[ind];
                     basicBot.room.roulette.participants = [];
-                    var pos = Math.floor((Math.random() * API.getWaitList().length) + 1);
                     var user = basicBot.userUtilities.lookupUser(winner);
                     var name = user.username;
-                    API.sendChat(subChat(basicBot.chat.winnerpicked, {name: name, position: pos}));
+                    API.sendChat(subChat(basicBot.chat.winnerpicked, {name: name}));
                     setTimeout(function (winner, pos) {
-                        basicBot.userUtilities.moveUser(winner, pos, false);
-                    }, 1 * 1000, winner, pos);
+                        basicBot.userUtilities.moveUser(winner, 1, false);
+                    }, 1 * 1000, winner);
                 }
             }*/
         },
