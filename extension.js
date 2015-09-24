2655db5707a22895385a8215a61bbec42a949173
WorstUdyrDE <WorstUdyrDE@users.noreply.github.com>
2015-09-24 16:06:45 +0200

    Miłość i życie

--- extension.js
+++ extension.js
@@ -189,7 +189,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                 if (!bot.commands.executable(this.rank, chat)) return void(0);
                 else {
-                    API.sendChat("/me @Wumekk jest miloscia, @Wumekk jest zyciem <3");
+                    API.sendChat("/me @Wumekk jest miłością, @Wumekk jest życiem <3");
                 }
             }
         };
